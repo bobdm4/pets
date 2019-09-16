b7e6f98a869e11553adc3b1f14e224033d7f432f
bor9sik <asdf514asdf514@gmail.com>
2019-09-16 14:17:48 +0300

    Updates

---
+++ precache-manifest.9d846eb6cb8310d2b29f8b655808318a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4da42ce40011dd14430c",
+    "url": "/pets/static/js/runtime~main.9ec6c824.js"
+  },
+  {
+    "revision": "14cf16245b7139347fdf",
+    "url": "/pets/static/js/main.a48456d4.chunk.js"
+  },
+  {
+    "revision": "a8fe43774d6a8cf50e35",
+    "url": "/pets/static/js/2.653250ec.chunk.js"
+  },
+  {
+    "revision": "14cf16245b7139347fdf",
+    "url": "/pets/static/css/main.13a9b9c9.chunk.css"
+  },
+  {
+    "revision": "e68a1a6548115c1042f7662a78c1a9cf",
+    "url": "/pets/index.html"
+  }
+];
